740bb3f66933b202342d7e691524e4271b84947c
Simon Hanley <sime.hanley@gmail.com>
2015-10-12 23:12:57 +1100

    gui sanbox

--- ezeeguisandbox/src/com/ezee/client/Ezeeguisandbox.java
+++ ezeeguisandbox/src/com/ezee/client/Ezeeguisandbox.java
@@ -1,8 +1,28 @@
 package com.ezee.client;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.ezee.client.main.EzeeInvoiceMain;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class Ezeeguisandbox implements EntryPoint {
+
+	private static final Logger log = Logger.getLogger("EzeeInvoiceWeb");
+
 	public void onModuleLoad() {
+		init();
+	}
+
+	private void init() {
+		initMain();
+	}
+
+	private void initMain() {
+		log.log(Level.INFO, "Initialising application.");
+		EzeeInvoiceMain main = new EzeeInvoiceMain();
+		RootLayoutPanel.get().add(main);
+		log.log(Level.INFO, "Application initialised.");
 	}
 }
