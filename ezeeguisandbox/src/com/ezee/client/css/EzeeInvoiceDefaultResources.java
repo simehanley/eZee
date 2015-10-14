8012fc3c8d964d5aea2c17c10908d68b120ca9ec
Simon Hanley <sime.hanley@gmail.com>
2015-10-14 21:16:52 +1100

    Sandbox

---
+++ ezeeguisandbox/src/com/ezee/client/css/EzeeInvoiceDefaultResources.java
@@ -0,0 +1,43 @@
+package com.ezee.client.css;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+/**
+ * 
+ * @author siborg
+ *
+ */
+public interface EzeeInvoiceDefaultResources extends ClientBundle {
+
+	public static EzeeInvoiceDefaultResources INSTANCE = GWT.create(EzeeInvoiceDefaultResources.class);
+
+	interface EzeeInvoiceDefaultCss extends CssResource {
+
+		String lightgray();
+
+		String lightblue();
+
+		String mediumblue();
+
+		String green();
+
+		String lightorange();
+
+		String header();
+
+		String headerLabel();
+
+		String footer();
+
+		String footerLabel();
+
+		String navigation();
+		
+		String navigationLink();
+	}
+
+	@Source("EzeeInvoiceDefault.css")
+	EzeeInvoiceDefaultCss css();
+}
---
+++ ezeeguisandbox/src/com/ezee/client/grid/EzeeInvoiceGrid.java
@@ -0,0 +1,31 @@
+package com.ezee.client.grid;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.DataGrid;
+
+public class EzeeInvoiceGrid<T> extends Composite {
+
+	private static EzeeInvoiceGridUiBinder uiBinder = GWT.create(EzeeInvoiceGridUiBinder.class);
+
+	@UiField
+	DataGrid<T> grid = new DataGrid<T>();
+
+	interface EzeeInvoiceGridUiBinder extends UiBinder<Widget, EzeeInvoiceGrid<?>> {
+	}
+
+	public EzeeInvoiceGrid() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	public final DataGrid<T> getGrid() {
+		return grid;
+	}
+
+	public void setGrid(DataGrid<T> grid) {
+		this.grid = grid;
+	}
+}
