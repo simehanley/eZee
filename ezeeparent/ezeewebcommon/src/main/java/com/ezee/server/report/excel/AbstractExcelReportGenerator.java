dcb6f70572b3f30f39ad54511038261eb09962f2
Simon Hanley <sime.hanley@gmail.com>
2015-11-10 00:26:07 +1100

    Excel report

--- ezeeparent/ezeewebcommon/src/main/java/com/ezee/server/report/excel/AbstractExcelReportGenerator.java
+++ ezeeparent/ezeewebcommon/src/main/java/com/ezee/server/report/excel/AbstractExcelReportGenerator.java
@@ -1,9 +1,17 @@
 package com.ezee.server.report.excel;
 
+import static org.apache.poi.ss.usermodel.CellStyle.ALIGN_CENTER;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import com.ezee.server.report.AbstractReportGenerator;
 
@@ -14,6 +22,33 @@ import com.ezee.server.report.AbstractReportGenerator;
  */
 public abstract class AbstractExcelReportGenerator extends AbstractReportGenerator {
 
+	protected int INVOICE_ID_INDEX = 0;
+	protected int SUPPLIER_INDEX = 1;
+	protected int PREMISES_INDEX = 2;
+	protected int CLASSIFICATION_INDEX = 3;
+	protected int AMOUNT_INDEX = 4;
+	protected int TAX_INDEX = 5;
+	protected int TOTAL_INDEX = 6;
+	protected int INVOICE_DATE_INDEX = 7;
+	protected int DUE_DATE_INDEX = 8;
+	protected int PAYMENT_DATE_INDEX = 9;
+
+	protected String INVOICE_ID = "InvoiceId";
+	protected String SUPPLIER = "Supplier";
+	protected String PREMISES = "Premises";
+	protected String CLASSIFICATION = "Classification";
+	protected String AMOUNT = "Amount";
+	protected String TAX = "Tax";
+	protected String TOTAL = "Total";
+	protected String INVOICE_DATE = "Invoice Date";
+	protected String DUE_DATE = "Due Date";
+	protected String PAYMENT_DATE = "Payment Date";
+
+	protected int[] INVOICE_REPORT_INDEXES = { INVOICE_ID_INDEX, SUPPLIER_INDEX, PREMISES_INDEX, CLASSIFICATION_INDEX,
+			AMOUNT_INDEX, TAX_INDEX, TOTAL_INDEX, INVOICE_DATE_INDEX, DUE_DATE_INDEX, PAYMENT_DATE_INDEX };
+	protected String[] INVOICE_REPORT_FIELDS = { INVOICE_ID, SUPPLIER, PREMISES, CLASSIFICATION, AMOUNT, TAX, TOTAL,
+			INVOICE_DATE, DUE_DATE, PAYMENT_DATE };
+
 	protected OutputStream createExcelFilenameResponseHeader(final HttpServletResponse resp, final String filename)
 			throws IOException {
 		OutputStream stream = resp.getOutputStream();
@@ -21,5 +56,38 @@ public abstract class AbstractExcelReportGenerator extends AbstractReportGenerat
 		resp.addHeader("Content-Type", "application/vnd.ms-excel");
 		resp.setHeader("Content-Disposition", "attachement;filename=" + filename);
 		return stream;
+	}
+
+	protected CellStyle dateStyle(final Workbook book, final boolean bold) {
+		CellStyle style = book.createCellStyle();
+		DataFormat format = book.createDataFormat();
+		style.setDataFormat(format.getFormat("dd/MM/yyyy"));
+		return style;
+	}
+
+	protected CellStyle currencyStyle(final Workbook book, final boolean bold) {
+		CellStyle style = book.createCellStyle();
+		DataFormat format = book.createDataFormat();
+		style.setDataFormat(format.getFormat("$* #,##0.00"));
+		if (bold) {
+			style.setFont(boldFont(book));
+		}
+		return style;
+	}
+
+	protected CellStyle boldStyle(final Workbook book, final boolean centered) {
+		CellStyle style = book.createCellStyle();
+		style.setFont(boldFont(book));
+		if (centered) {
+			style.setAlignment(ALIGN_CENTER);
+		}
+		style.setWrapText(true);
+		return style;
+	}
+
+	protected Font boldFont(final Workbook book) {
+		Font font = book.createFont();
+		font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+		return font;
 	}
 }
--- ezeeparent/ezeewebcommon/src/main/java/com/ezee/server/report/excel/invoice/EzeeInvoiceReportGenerator.java
+++ ezeeparent/ezeewebcommon/src/main/java/com/ezee/server/report/excel/invoice/EzeeInvoiceReportGenerator.java
@@ -1,9 +1,17 @@
 package com.ezee.server.report.excel.invoice;
 
+import static com.ezee.common.EzeeCommonConstants.ONE;
+import static com.ezee.common.EzeeCommonConstants.ZERO;
+import static com.ezee.common.EzeeCommonConstants.ZERO_DBL;
 import static com.ezee.common.collections.EzeeCollectionUtils.isEmpty;
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_NUMERIC;
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -11,10 +19,17 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ezee.common.EzeeCommonConstants;
 import com.ezee.dao.EzeeInvoiceDao;
 import com.ezee.model.entity.EzeeInvoice;
 import com.ezee.server.report.EzeeReportGenerator;
@@ -39,7 +54,6 @@ public class EzeeInvoiceReportGenerator extends AbstractExcelReportGenerator imp
 		} catch (IOException exception) {
 			log.error("Error generating ezee invoice report.", exception);
 		}
-
 	}
 
 	private void generateEzeeInvoiceReport(HttpServletRequest request, HttpServletResponse response)
@@ -61,12 +75,105 @@ public class EzeeInvoiceReportGenerator extends AbstractExcelReportGenerator imp
 		stream.close();
 	}
 
-	private byte[] generateEzeeInvoiceReport(final Map<String, List<EzeeInvoice>> supplierInvoices) {
-		/* implement me */
-		return new byte[] {};
+	private byte[] generateEzeeInvoiceReport(final Map<String, List<EzeeInvoice>> supplierInvoices) throws IOException {
+		Workbook book = new HSSFWorkbook();
+		Sheet sheet = book.createSheet("invoices");
+		generateEzeeInvoiceReportHeader(book, sheet);
+		generateEzeeInvoiceReportContent(book, sheet, supplierInvoices);
+		formatReport(sheet);
+		ByteArrayOutputStream result = new ByteArrayOutputStream();
+		book.write(result);
+		result.close();
+		return result.toByteArray();
+	}
+
+	private void formatReport(final Sheet sheet) {
+	}
+
+	private void generateEzeeInvoiceReportContent(final Workbook book, final Sheet sheet,
+			final Map<String, List<EzeeInvoice>> supplierInvoices) {
+		int row = ONE;
+		for (String key : supplierInvoices.keySet()) {
+			row += generateEzeeInvoiceReportContent(book, sheet, supplierInvoices.get(key), row);
+		}
+	}
+
+	private int generateEzeeInvoiceReportContent(final Workbook book, final Sheet sheet,
+			final List<EzeeInvoice> invoices, int row) {
+		double amount = ZERO_DBL, tax = ZERO_DBL, total = ZERO_DBL;
+		CellStyle currencyStyle = currencyStyle(book, false);
+		CellStyle boldCurrencyStyle = currencyStyle(book, true);
+		CellStyle boldStyle = boldStyle(book, false);
+		String supplierName = invoices.get(EzeeCommonConstants.ZERO).getPayee().getName();
+		for (EzeeInvoice invoice : invoices) {
+			Row newrow = sheet.createRow(row);
+			Cell invoiceNum = newrow.createCell(INVOICE_ID_INDEX, CELL_TYPE_STRING);
+			invoiceNum.setCellValue(invoice.getInvoiceId());
+			Cell supplier = newrow.createCell(SUPPLIER_INDEX, CELL_TYPE_STRING);
+			supplier.setCellValue(invoice.getPayee().getName());
+			Cell premises = newrow.createCell(PREMISES_INDEX, CELL_TYPE_STRING);
+			premises.setCellValue(invoice.getPayer().getName());
+			Cell classification = newrow.createCell(CLASSIFICATION_INDEX, CELL_TYPE_STRING);
+			classification.setCellValue(invoice.getClassification().toString());
+			Cell invoiceAmount = newrow.createCell(AMOUNT_INDEX, CELL_TYPE_NUMERIC);
+			invoiceAmount.setCellValue(invoice.getAmount());
+			invoiceAmount.setCellStyle(currencyStyle);
+			Cell invoiceTax = newrow.createCell(TAX_INDEX, CELL_TYPE_NUMERIC);
+			invoiceTax.setCellValue(invoice.getTax());
+			invoiceTax.setCellStyle(currencyStyle);
+			Cell invoiceTotal = newrow.createCell(TOTAL_INDEX, CELL_TYPE_NUMERIC);
+			invoiceTotal.setCellValue(invoice.getInvoiceAmount());
+			invoiceTotal.setCellStyle(currencyStyle);
+			Cell invoiceDate = newrow.createCell(INVOICE_DATE_INDEX, CELL_TYPE_STRING);
+			invoiceDate.setCellValue(invoice.getInvoiceDate());
+			Cell dueDate = newrow.createCell(DUE_DATE_INDEX, CELL_TYPE_STRING);
+			dueDate.setCellValue(invoice.getDateDue());
+			Cell paymentDate = newrow.createCell(PAYMENT_DATE_INDEX, CELL_TYPE_STRING);
+			paymentDate.setCellValue(invoice.getDatePaid());
+			amount += invoice.getAmount();
+			tax += invoice.getTax();
+			total += invoice.getInvoiceAmount();
+			row += ONE;
+		}
+		row += ONE;
+		Row totalrow = sheet.createRow(row);
+		Cell totalSummary = totalrow.createCell(INVOICE_ID_INDEX, CELL_TYPE_STRING);
+		totalSummary.setCellStyle(boldStyle);
+		totalSummary.setCellValue("Totals for '" + supplierName + " :");
+		Cell totalAmount = totalrow.createCell(AMOUNT_INDEX, CELL_TYPE_NUMERIC);
+		totalAmount.setCellStyle(boldCurrencyStyle);
+		totalAmount.setCellValue(amount);
+		Cell totalTax = totalrow.createCell(TAX_INDEX, CELL_TYPE_NUMERIC);
+		totalTax.setCellStyle(boldCurrencyStyle);
+		totalTax.setCellValue(tax);
+		Cell grandTotal = totalrow.createCell(TOTAL_INDEX, CELL_TYPE_NUMERIC);
+		grandTotal.setCellStyle(boldCurrencyStyle);
+		grandTotal.setCellValue(total);
+		row += ONE;
+		return row;
+	}
+
+	private void generateEzeeInvoiceReportHeader(final Workbook book, final Sheet sheet) {
+		Row header = sheet.createRow(ZERO);
+		int[] indexes = INVOICE_REPORT_INDEXES;
+		String[] fields = INVOICE_REPORT_FIELDS;
+		CellStyle headerStyle = boldStyle(book, true);
+		for (int i = ZERO; i < indexes.length; i++) {
+			Cell cell = header.createCell(i, CELL_TYPE_STRING);
+			cell.setCellValue(fields[i]);
+			cell.setCellStyle(headerStyle);
+		}
 	}
 
 	private Map<String, List<EzeeInvoice>> resolveInvoicesBySupplier(final List<EzeeInvoice> invoices) {
-		return null;
+		Map<String, List<EzeeInvoice>> supplierInvoices = new HashMap<>();
+		for (EzeeInvoice invoice : invoices) {
+			String key = invoice.getPayee().getName();
+			if (!supplierInvoices.containsKey(key)) {
+				supplierInvoices.put(key, new ArrayList<>());
+			}
+			supplierInvoices.get(key).add(invoice);
+		}
+		return supplierInvoices;
 	}
 }
