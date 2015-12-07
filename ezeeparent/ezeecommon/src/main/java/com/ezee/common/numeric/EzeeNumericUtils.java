fb35d498675a4030fb7e10a018f7560923285b08
Simon Hanley <sime.hanley@gmail.com>
2015-12-07 18:19:21 +1100

    changes.

--- ezeeparent/ezeecommon/src/main/java/com/ezee/common/numeric/EzeeNumericUtils.java
+++ ezeeparent/ezeecommon/src/main/java/com/ezee/common/numeric/EzeeNumericUtils.java
@@ -11,11 +11,17 @@ import java.math.BigDecimal;
  */
 public class EzeeNumericUtils {
 
+	private static final double ZERO_TOLERANCE = 1e-12;
+
 	public static double round(final double value) {
 		return round(value, 2);
 	}
 
 	public static double round(final double value, final int places) {
 		return new BigDecimal(value).setScale(places, HALF_EVEN).doubleValue();
+	}
+
+	public static boolean isCloseToZero(final double value) {
+		return Math.abs(value) < ZERO_TOLERANCE;
 	}
 }
